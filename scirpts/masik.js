4f75b159b0930d5c30983cf66873aeabfe8a4b6e
Zsombee <hornyik.zsombor09@gmail.com>
2026-03-09 19:12:58 +0100

    moreinfo kiirasa htmlbe

--- scirpts/masik.js
+++ scirpts/masik.js
@@ -1,4 +1,3 @@
-
 const capital = document.getElementById("capital");
 
 
@@ -12,8 +11,30 @@ async function getCountry(){
     const res = await fetch(`https://restcountries.com/v3.1/translation/${encodeURIComponent(country)}`);
 
     const data = await res.json();
-    
-    capital.innerHTML = `Capital: ${data[0].capital?.[0] || "N/A"}`
+    const c = data[0];
+
+    document.getElementById("countryName").innerText = c.name.common;
+    document.getElementById("flagImg").src = c.flags.png;
+    document.getElementById("capital").innerText = c.capital ? c.capital[0] : "N/A";
+    document.getElementById("population").innerText = c.population.toLocaleString('hu-HU');
+        
+    document.getElementById("region").innerText = `${c.region} (${c.subregion})`;
+    document.getElementById("area").innerText = `${c.area.toLocaleString('hu-HU')} km²`;
+    if (c.currencies) {
+        const currencyKey = Object.keys(c.currencies)[0];
+        document.getElementById("currency").innerText = `${c.currencies[currencyKey].name} (${c.currencies[currencyKey].symbol})`;
+    }
+
+        
+    if (c.languages) {
+        document.getElementById("language").innerText = Object.values(c.languages).join(", ");
+    }      
+    document.getElementById("timezone").innerText = c.timezones[0];
+    if (c.coatOfArms && c.coatOfArms.png) {
+        document.getElementById("armsImg").src = c.coatOfArms.png;
+    } else {
+        document.getElementById("armsImg").style.display = "none";
+    }
 }
 
 
